81158a6b9d9d3ff9c5f67f2eeee2be0b7836f045
Chiqua-glitch <quentin.cheron26200@gmail.com>
2024-01-18 10:57:39 +0100

    add headerNav

---
+++ components/HeaderNav.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const HeaderNav = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        Test
+                    </li>
+                    <li>
+                        Test
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default HeaderNav;
