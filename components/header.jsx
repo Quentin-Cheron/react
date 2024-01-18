62f1b39815dfc66eb3f24d78bfecd12371bb8d6c
Chiqua-glitch <quentin.cheron26200@gmail.com>
2024-01-18 10:56:07 +0100

    add menu

---
+++ components/header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const header = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    Home
+                </li>
+                <li>
+                    About
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default header;
